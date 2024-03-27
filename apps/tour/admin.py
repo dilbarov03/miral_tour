643e9bca9f84c6c116be2b9a7703dcec98ce5163
dilbarov03 <dilbarov03@gmail.com>
2024-03-27 13:41:28 +0500

    tour app admin

--- apps/tour/admin.py
+++ apps/tour/admin.py
@@ -1,3 +1,75 @@
 from django.contrib import admin
+from modeltranslation.admin import TabbedTranslationAdmin, TranslationTabularInline, TranslationStackedInline
+from .models import Tour, Region, TourCategory, TourType, TourDays, Feature, TourTarif, TarifFeature, TourImage, \
+    TourFeature
 
-# Register your models here.
+
+@admin.register(Region)
+class RegionAdmin(TabbedTranslationAdmin):
+    list_display = ("id", "name", "order")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(TourCategory)
+class TourCategoryAdmin(TabbedTranslationAdmin):
+    list_display = ("id", "name", "order")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(TourType)
+class TourTypeAdmin(TabbedTranslationAdmin):
+    list_display = ("id", "name", "order")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Feature)
+class FeatureAdmin(TabbedTranslationAdmin):
+    list_display = ("id", "title", "text")
+    list_display_links = ("id", "title")
+    search_fields = ("title", "text")
+
+
+class TourImagesInline(admin.TabularInline):
+    model = TourImage
+    extra = 0
+
+
+class TourDaysInline(TranslationStackedInline):
+    model = TourDays
+    extra = 0
+
+
+class TourFeatureInline(admin.TabularInline):
+    model = TourFeature
+    extra = 0
+
+
+class TourTarifInline(TranslationStackedInline):
+    model = TourTarif
+    extra = 0
+
+
+@admin.register(Tour)
+class TourAdmin(TabbedTranslationAdmin):
+    list_display = ("id", "title", "category", "tour_type")
+    list_display_links = ("id", "title")
+    search_fields = ("title",)
+    list_filter = ("category", "tour_type", "from_region", "to_region", "return_region",
+                   "from_date", "to_date", "is_active", "discount")
+    inlines = (TourImagesInline, TourDaysInline, TourFeatureInline, TourTarifInline)
+
+
+class TarifFeatureInline(TranslationTabularInline):
+    model = TarifFeature
+
+
+@admin.register(TourTarif)
+class TourTarifAdmin(TabbedTranslationAdmin):
+    list_display = ("id", "title", "tour")
+    list_display_links = ("id", "title")
+    search_fields = ("title", "tour__title")
+    list_filter = ("tour",)
+    inlines = (TarifFeatureInline,)
---
+++ apps/tour/translation.py
@@ -0,0 +1,44 @@
+from modeltranslation import translator
+from modeltranslation.translator import TranslationOptions
+
+from .models import Tour, Region, TourCategory, TourType, TourDays, Feature, TourTarif, TarifFeature
+
+
+@translator.register(Region)
+class RegionTranslationOptions(TranslationOptions):
+    fields = ("name",)
+
+
+@translator.register(TourCategory)
+class TourCategoryTranslationOptions(TranslationOptions):
+    fields = ("name",)
+
+
+@translator.register(TourType)
+class TourTypeTranslationOptions(TranslationOptions):
+    fields = ("name",)
+
+
+@translator.register(Tour)
+class TourTranslationOptions(TranslationOptions):
+    fields = ("title", "discount_text")
+
+
+@translator.register(TourDays)
+class TourDaysTranslationOptions(TranslationOptions):
+    fields = ("title", "text")
+
+
+@translator.register(Feature)
+class FeatureTranslationOptions(TranslationOptions):
+    fields = ("title", "text")
+
+
+@translator.register(TourTarif)
+class TourTarifTranslationOptions(TranslationOptions):
+    fields = ("title",)
+
+
+@translator.register(TarifFeature)
+class TarifFeatureTranslationOptions(TranslationOptions):
+    fields = ("value",)
